405d8519c53752f01b9bf4d298efc9dfcfb0962e
Scott Mackenzie <mackes13@gmail.com>
2017-09-11 17:25:10 +1200

    rolledback

--- api/settings.py
+++ api/settings.py
@@ -142,6 +142,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': 'django_filters.rest_framework.DjangoFilterBackend',
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
     ],
--- booking/views.py
+++ booking/views.py
@@ -1,5 +1,7 @@
 import rest_framework.viewsets as rest_viewsets
 import rest_framework.permissions as rest_permissions
+from rest_framework.filters import OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend, FilterSet
 
 import booking.models as booking_models
 import booking.permissions as booking_permissions
@@ -10,6 +12,8 @@ import booking.serializers as booking_serializers
 class BookingView(rest_viewsets.ModelViewSet):
     queryset = booking_models.Booking.objects.all()
     serializer_class = booking_serializers.BookingSerializer
+    filter_backends = (DjangoFilterBackend, OrderingFilter,)
+    ordering_fields = '__all__'
 
     permissions = [
         booking_permissions.CanBook,
@@ -23,7 +27,7 @@ class BookingView(rest_viewsets.ModelViewSet):
         queryset = self.filter_queryset_on_get(queryset, 'room')
         queryset = self.filter_queryset_on_get(queryset, 'status')
 
-        return queryset.order_by('room','-created')
+        return queryset
 
     def filter_queryset_on_get(self, queryset, key):
         value = self.request.query_params.get(key, None)
