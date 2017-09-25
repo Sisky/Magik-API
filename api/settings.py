80f9993aaa818dc0ba76fe6c62dcbe4db5e7e036
Scott Mackenzie <mackes13@gmail.com>
2017-09-25 18:15:23 +1300

    edit page size

--- api/settings.py
+++ api/settings.py
@@ -146,7 +146,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
     ],
-    'PAGE_SIZE': 1000,
+    'PAGE_SIZE': 100000,
 }
 
 # Heroku: Update database configuration from $DATABASE_URL.
